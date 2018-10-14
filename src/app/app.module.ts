b6ee4ba22fd4141e9c1bc8119c161012e8f32f14
pevargasg <wearetamo@gmail.com>
2018-10-14 04:22:26 -0700

    injected movie component to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import {AngularFireModule} from 'angularfire2'
 import { AngularFirestoreModule} from 'angularfire2/firestore'
 import { environment } from '../environments/environment'
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
