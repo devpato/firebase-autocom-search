b7b4f3278f55dd4097af068169d260eee03a7286
pevargasg <wearetamo@gmail.com>
2018-10-14 04:16:19 -0700

    imported firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
+import {AngularFireModule} from 'angularfire2'
+import { AngularFirestoreModule} from 'angularfire2/firestore'
+import { environment } from '../environments/environment'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence()
   ],
   providers: [],
   bootstrap: [AppComponent]
