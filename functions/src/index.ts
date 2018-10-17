c9168300e29bf1871c891e95aa81fa0da7ea29cc
pevargasg <wearetamo@gmail.com>
2018-10-16 22:10:11 -0700

    cloud function using angolia done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,35 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const env = functions.config();
+
+import * as algoliasearch from "algoliasearch";
+
+// Initialize the Algolia Client
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex("movies_search");
+
+exports.indexMovie = functions.firestore
+  .document("movies/{movielId}")
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const objectID = snap.id;
+
+    // Add the data to the algolia index
+    return index.addObject({
+      objectID,
+      ...data
+    });
+  });
+
+exports.unindexMovie = functions.firestore
+  .document("movies/{movielId}")
+  .onDelete((snap, context) => {
+    const objectId = snap.id;
+
+    // Delete an ID from the index
+    return index.deleteObject(objectId);
+  });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
